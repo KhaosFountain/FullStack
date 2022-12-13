d13e79c3091fcccdfaa6a0e46a86bd59c5d02f0f
KhaosFountain <g00383507@atu.ie>
2022-12-13 23:12:45 +0000

    made changes to meetup details

--- components/meetups/MeetupDetail.js
+++ components/meetups/MeetupDetail.js
@@ -1,13 +1,12 @@
 import styles from "./MeetupDetail.module.css";
 import React, { useState } from "react";
-import Popup from "reactjs-popup";
 
 function MeetupDetail(props) {
 
-    const [modal, setModal] = useState(false);
-    const Option1 = () => {
-        setModal(!modal);
-    }
+    // const [modal, setModal] = useState(false);
+    // const Option1 = () => {
+    //     setModal(!modal);
+    // }
 
   return (
     <>
@@ -20,7 +19,7 @@ function MeetupDetail(props) {
         </div>
 
         <div className={styles.carPrice}>
-          <div className={styles.priceCard} onClick={Option1()}>
+          <div className={styles.priceCard} >
             <div className={styles.cardDetail}>
               <h4>
                 <b>Option 1</b>
